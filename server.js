b2492a7a014d3dd98813c0c7485dc9d5185d1704
ShoxruxMirzo23 <shohrukh_2019@mail.ru>
2024-02-07 17:04:34 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 // 1: Kirish kodlar
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parsel(data);
+  }
+});
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session kodlar
-
 // 3Views kodlar
+
 app.set("views", "viewws");
 app.set("view engine", "ejs");
 
 // 4: Routing kodlar
 app.get("/hello", function (req, res) {
   res.end(`<h1>HELLO WORLD</h1>`);
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 app.get("/gift", function (req, res) {
   res.end(`<h1>siz sovgalar bolimidasiz</h1>`);
