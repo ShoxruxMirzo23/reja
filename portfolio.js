3aa06a8c5ac1cfaf764bf8ad2ba1c749f77093c7
ShoxruxMirzo23 <shohrukh_2019@mail.ru>
2024-02-19 04:04:50 +0900

    Portfolio done

---
+++ portfolio.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+//1 enter code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2 session code
+
+//3 views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 Routing code
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+
+server.listen(PORT, function () {
+  console.log(`The server is running succesfully on port ${PORT}`);
+});
